079c543d275aff0e77778136591dedba68faf44c
humblemouse <javascriptjenkins@gmail.com>
2021-10-28 16:08:47 -0500

    upvote downvote works.

---
+++ src/main/java/com/isaac/collegeapp/viewcontroller/CancelTrainViewController.java
@@ -0,0 +1,154 @@
+package com.isaac.collegeapp.viewcontroller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.isaac.collegeapp.h2model.CancelTrainVO;
+import com.isaac.collegeapp.jparepo.CancelTrainRepo;
+import com.isaac.collegeapp.model.StudentDAO;
+import com.isaac.collegeapp.service.StudentService;
+import com.isaac.collegeapp.util.ControllerHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequestMapping("/")
+@Controller
+public class CancelTrainViewController {
+
+    @Autowired
+    StudentService studentService;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    ControllerHelper controllerHelper;
+
+    @Autowired
+    HttpServletRequest httpServletRequest;
+
+    @Autowired
+    CancelTrainRepo cancelTrainRepo;
+
+
+    //default home mapping
+    @GetMapping
+    String viewHomePage(Model model){
+
+//        controllerHelper.checkForLoggedInStudent(model, httpServletRequest); // this will check to see if a student has already loggede in
+        model.addAttribute("canceltrains", cancelTrainRepo.findAll());
+        model.addAttribute("canceltrain", new CancelTrainVO());
+
+
+//        model.addAttribute("students", studentService.getAllStudentData());
+        return "index.html";
+    }
+
+
+    @PostMapping("/vote")
+    String vote(@ModelAttribute( "canceltrain" ) CancelTrainVO cancelTrainVO, Model model){
+
+        if(cancelTrainVO.getIncomingVote() ==0){
+            Optional<CancelTrainVO> existing = cancelTrainRepo.findById(cancelTrainVO.getId());
+
+            existing.get().setDownvotes(existing.get().getDownvotes() - 1);
+            cancelTrainRepo.save(existing.get());
+
+        } else if(cancelTrainVO.getIncomingVote() ==1){
+            Optional<CancelTrainVO> existing = cancelTrainRepo.findById(cancelTrainVO.getId());
+            existing.get().setUpvotes(existing.get().getUpvotes() + 1);
+            cancelTrainRepo.save(existing.get());
+
+        }
+
+
+
+        model.addAttribute("canceltrain", new CancelTrainVO());
+
+        model.addAttribute("canceltrains",cancelTrainRepo.findAll());
+
+
+        return "index.html";
+    }
+
+
+    @GetMapping("/viewStudents")
+    String viewStudents(Model model){
+
+        controllerHelper.checkForLoggedInStudent(model, httpServletRequest); // this will check to see if a student has already loggede in
+
+
+        model.addAttribute("students", studentService.getAllStudentData());
+        return "viewStudents.html";
+    }
+
+    @GetMapping("/editStudent")
+    String editStudent(Model model){
+        controllerHelper.checkForLoggedInStudent(model, httpServletRequest); // this will check to see if a student has already loggede in
+
+
+        model.addAttribute("student", new StudentDAO());
+        model.addAttribute("students",studentService.getAllStudentData());
+        return "editStudent.html";
+    }
+
+    @PostMapping("/submitEditStudent")
+    String submitEditStudent(@ModelAttribute( "student" ) StudentDAO studentDAO, Model model){
+
+        controllerHelper.checkForLoggedInStudent(model, httpServletRequest); // this will check to see if a student has already loggede in
+
+
+        System.out.println(studentDAO);
+
+
+        // now the edit works, just need to submit the edit to the database
+
+        model.addAttribute("student", new StudentDAO());
+
+        model.addAttribute("students",studentService.getAllStudentData());
+
+
+        return "editStudent.html";
+    }
+
+    @GetMapping("/newStudent")
+    String newStudent(Model model){
+        controllerHelper.checkForLoggedInStudent(model, httpServletRequest); // this will check to see if a student has already loggede in
+
+
+        model.addAttribute("student", new StudentDAO());
+        model.addAttribute("students",studentService.getAllStudentData());
+        return "newStudent.html";
+    }
+
+    @PostMapping ("/createStudent")
+    String createStudent(@ModelAttribute( "student" ) StudentDAO studentDAO, Model model){
+
+        controllerHelper.checkForLoggedInStudent(model, httpServletRequest); // this will check to see if a student has already loggede in
+
+
+        // Validation on student name
+        if(studentDAO.getStudentName().length() > 60){
+            model.addAttribute("errorMessage","Student Name must be shorter than 60 characters");
+        } else {
+            // This code block will execute if there are no errors in the data that was inputed by the client
+
+            // step 1) create the new student and attach the success message
+            String result = studentService.createStudent(studentDAO);
+            model.addAttribute("successMessage",result);
+
+            // step 2) fetch the list of students from the database and bind the list onto the page
+            model.addAttribute("students",studentService.getAllStudentData());
+
+        }
+
+        return "newStudent.html";
+    }
+
+
+}
